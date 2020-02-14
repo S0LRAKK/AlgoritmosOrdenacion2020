8b273fcebe4302b81ecfae2ebb64131b352bd27e
xp <xp@CETYSDAM00>
2020-02-14 16:15:14 +0100

    empezado

---
+++ AlgoritmosOrdenacion2020/src/algoritmosordenacion2020/AlgoritmosOrdenacion2020.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package algoritmosordenacion2020;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author Carlos
+ */
+public class AlgoritmosOrdenacion2020 {
+    
+    
+    int [] lista1 = {13, 27, 455, 621, 23, 1, 15}
+    
+    
+    public void ordenacionBurbuja(int [] numeros){
+        //el método recibe un array de números
+        //y lo ordenará mediante el algoritmo de la burbuja
+        for (int j=0; j<numeros.length; j++){
+            for (int i=0; i<numeros.length; i++){
+                if(numeros[i] > numeros[i+1]){
+                //si se cumple, intercambio los valores de i e i+1
+                int aux = numeros [i+1];
+                numeros[i+1] = numeros[i];
+                numeros[i] = aux;
+            }
+        }
+        
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        AlgoritmosOrdenacion2020 ordenacion = new AlgoritmosOrdenacion2020
+                
+                ordenacion.ordenacionBurbuja(ordenacion.lista1);
+                
+                System.out.println("lista ordenada" + Arrays.toString(ordenacion.lista1));
+    }
+    
+}
