bfbd28803bbe3f081ffdb5fdd1cfc9c7fad14d1d
xp <xp@CETYSDAM00>
2020-02-18 17:00:56 +0100

    con el shell sort mal

--- AlgoritmosOrdenacion2020/src/algoritmosordenacion2020/AlgoritmosOrdenacion2020.java
+++ AlgoritmosOrdenacion2020/src/algoritmosordenacion2020/AlgoritmosOrdenacion2020.java
@@ -6,6 +6,7 @@
 package algoritmosordenacion2020;
 
 import java.util.Arrays;
+import java.util.Random;
 
 /**
  *
@@ -14,7 +15,10 @@ import java.util.Arrays;
 public class AlgoritmosOrdenacion2020 {
 
     
-    int [] lista1 = {13, 27, 455, 621, 23, 1, 15}; 
+    int [] lista1 = {13, 27, 455, 621, 23, 1, 15};
+    int [] arrayParaBurbuja;
+    int [] arrayParaInsercion;
+    int [] arrayParaShell;
              
              
     public void ordenacionBurbuja(int[] numeros) {
@@ -32,7 +36,49 @@ public class AlgoritmosOrdenacion2020 {
         }
     }
     
+    public void ordenacionInsercion(int[] numeros){
+        for (int i=2; i< numeros.length; i++){
+            int aux=numeros[i];
+            int j=0;
+            for (j = i-1; j>= 0 && numeros[j]>aux; j--){
+                numeros[j+1] = numeros[j];
+            }
+            numeros[j+1] = aux;
+        }
+    }
     
+    public void  shellSort (int [] numeros){
+        int salto, aux;
+        boolean intercambio;
+        for (salto = numeros.length /2; salto!= 0; salto /= 2){
+            intercambio = true;
+            while (intercambio){
+                intercambio = false;
+                for (int i= salto; i<numeros.length; i += salto){
+                    if (numeros [i-salto] > numeros[i]){
+                        //si los dos numeros estan desordenados entre si
+                        //los intercambio y lo indico
+                        aux=numeros[i];
+                        numeros[i] = numeros[i - salto];
+                        numeros[i-salto] = aux;
+                        intercambio = true;
+                    }
+                }
+            }
+        }
+    }
+    
+    //crea un array de tantos números aleatorios como
+    //le pasemos en la variable dimension
+    public int[] generaNumerosRandom (int dimension){
+        int[] numeros = new int[dimension];
+        Random r = new Random();
+        for (int i=0; i< dimension; i++){
+            numeros[i] = r.nextInt();            
+        }
+        return numeros;
+    }
+
    
     
 
@@ -49,7 +95,50 @@ public class AlgoritmosOrdenacion2020 {
         System.out.println("lista ordenada:" 
                 + Arrays.toString(ordenacion.lista1));
         
+        /////////empiezo las pruebas de rendimiento de los
+        //////// distintos algoritmos
+        int rangoPrueba = 100000;  //numero de datos con los que probamos
+        int[] numeros = ordenacion.generaNumerosRandom(rangoPrueba);
+        ordenacion.arrayParaBurbuja = new int[rangoPrueba];
+        ordenacion.arrayParaInsercion = new int[rangoPrueba];
+        for (int i=0; i<rangoPrueba; i++){
+            ordenacion.arrayParaBurbuja[i] = numeros[i];
+            ordenacion.arrayParaInsercion[i] = numeros[i];
+        }
+        //aquí ya tengo dos copias exactas del array de datos aleatorios
         
+        System.out.println("Empieza la burbuja: (vete a por un café)");
+        long tiempoInicio = System.currentTimeMillis();
+        
+        ordenacion.ordenacionBurbuja(ordenacion.arrayParaBurbuja);
+        
+        long tiempoFinal = System.currentTimeMillis();
+        
+        System.out.println("La burbuja ha tardado: "+ (tiempoFinal - tiempoInicio));
+        
+        ///////////////////////////////////////////////////////////////////
+        
+        System.out.println("Empieza Inserción Directa: ");
+        tiempoInicio = System.currentTimeMillis();
+        
+        ordenacion.ordenacionInsercion(ordenacion.arrayParaInsercion);
+        
+        tiempoFinal = System.currentTimeMillis();
+        
+        System.out.println("La Inserción Directa ha tardado: "+ (tiempoFinal - tiempoInicio));
+        
+        ///////////////////////////////////////////////////////////////////
+        
+        System.out.println("Empieza Shell Sort: ");
+        tiempoInicio = System.currentTimeMillis();
+        
+        ordenacion.ordenacionShellSort(ordenacion.arrayParaShell);
+        
+        tiempoFinal = System.currentTimeMillis();
+        
+        System.out.println("Shell Sort ha tardado: "+ (tiempoFinal - tiempoInicio));
+        
+        ///////////////////////////////////////////////////////////////////
     }
 
 }
